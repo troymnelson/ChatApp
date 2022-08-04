a3288fa6a810d4601fa912efa8e7f26cd71651cd
jonah lindsley <jonahlindsley@yahoo.com>
2022-08-04 14:31:10 -0500

    added user model and db-connection

---
+++ models/user.js
@@ -0,0 +1,37 @@
+
+const { Model, DataTypes } = require('sequelize');
+
+const sequelize = require('../config/db-connection');
+
+class User extends Model {}
+
+
+User.init(
+  {
+  id: {
+    Type: DataTypes.NUMBER,
+    allowNullq: false,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  name: {
+    Type: DataTypes.STRING,
+    allowNull: false
+  },
+  email: {
+    Type: DataTypes.STRING,
+    allowNull: false
+  }
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'product',
+  }
+);
+
+
+
+module.exports = User;
