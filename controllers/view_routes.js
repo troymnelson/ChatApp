fba4eb02e348e3e31d8c00fd2a67cc338c8a1675
jonah lindsley <jonahlindsley@yahoo.com>
2022-08-08 13:19:42 -0500

    improved the handlebars

--- controllers/view_routes.js
+++ controllers/view_routes.js
@@ -3,15 +3,15 @@ const router = require('express').Router(); // REQUIRE DA ROUTER
 // const User = require('../models/User');
 
 router.get('/', (request, response) => { // RENDER INDEX.HTML ON ROOT ROUTE
-    response.render('index')
+    response.render('index', {title: 'Chat app', isHome: true})
 });
 
 router.get('/register', (request, response) => {
-    response.render('register');
+    response.render('register', {title: 'register', isHome: false});
 });
 
 router.get('/login', (req, res) => {
-    res.render('login');
+    res.render('login', {title: 'login', isHome: false});
 });
 
 
