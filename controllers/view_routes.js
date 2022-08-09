641d122f1afbb532cd557b87f8ad91b0daa8b12b
jonah lindsley <jonahlindsley@yahoo.com>
2022-08-09 14:45:02 -0500

    added party clock

--- controllers/view_routes.js
+++ controllers/view_routes.js
@@ -1,9 +1,13 @@
+var formatDistanceToNow = require('date-fns/formatDistanceToNow')
+const result = formatDistanceToNow(
+    new Date(2022, 7, 13),
+    {addSuffix: true})
 const User = require('../models/user');
 const db = require('../config/db-connection');
 const router = require('express').Router(); // REQUIRE DA ROUTER
 
 // const User = require('../models/User');
-
+console.log(result)
 router.get('/', (req, res) => { // RENDER INDEX.HTML ON ROOT ROUTE
     if (req.session.user_id){
        
@@ -30,7 +34,7 @@ router.get('/login', (req, res) => {
 
 router.get('/dashboard', (req, res) => {
         
-        res.render('dashboard', { title: 'Dashboard', isHome: false });
+        res.render('dashboard', { title: 'Dashboard', isHome: false, friday: result });
     
 
 });
