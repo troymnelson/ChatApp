6986c3bfc4ee14e4f2d0082870f2e4b1bf3025fa
Steve Lang <langloiss0003@my.uwstout.edu>
2022-08-09 10:08:25 -0500

    login working for sure

--- controllers/auth-routes.js
+++ controllers/auth-routes.js
@@ -25,28 +25,23 @@ auth_router.post('/registration', (req, res) => {
 })
 
 auth_router.post('/logged', (req, res) => {
-    const { email, password } = req.body
-
-    if (!email || !password) {
-        return res.redirect('/login')
-    }
     User.findOne({
         where: {
-            email
+            email: req.body.email
         }
     }).then(async user => {
         if (!user) {
             return res.redirect('/login')
         }
 
-        const pass_valid = await user.validatePassword(password, user.password)
+        const pass_valid = await user.validatePassword(req.body.password, user.password)
 
         if (!pass_valid) {
             return res.redirect('/login')
         }
 
         req.session.save(() => {
-            req.session.user_id = user_id;
+            req.session.user_id = user.id;
             res.redirect('/dashboard')
         })
     })
