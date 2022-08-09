8c76e59f0822a33914f802aa974f6b239bb133fc
unknown <troymnlsn@gmail.com>
2022-08-09 14:41:29 -0500

    FIXED EVERYTHING EVERYONE PULL

--- config/db-connection.js
+++ config/db-connection.js
@@ -1,18 +1,11 @@
 
 const { Sequelize } = require('sequelize');
-<<<<<<< HEAD
+
 require('dotenv').config();
 
 const connection = new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USER,
-=======
-require('dotenv').config()
-
-const connection = new Sequelize(
-    process.env.DB_NAME, 
-    process.env.DB_USER, 
->>>>>>> e78a542f8435b2eb1e8e862cf1fe7d2089b75853
     process.env.DB_PASSWORD,
     {
         host: 'localhost',
